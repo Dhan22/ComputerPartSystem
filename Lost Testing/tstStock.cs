c26606c3ebafffbd6e0456ec500739f06ad994aa
Dhanik Bhadresa <P2519702@my365.dmu.ac.uk>
2021-03-10 14:57:23 +0000

    Added testing and class for stock managment

---
+++ Lost Testing/tstStock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PartClasses;
+
+namespace Lost_Testing
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instacne of the class 
+            clsStock AnStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(AnStock);
+
+        }
+    }
+}
